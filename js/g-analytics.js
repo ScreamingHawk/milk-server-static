f108379ce84d947736f8bb87a1f8a07574ad70f6
Michael Standen <screaminghawk@gmail.com>
2015-11-24 07:36:45 +1300

    Moved analytics to js script

---
+++ js/g-analytics.js
@@ -0,0 +1,6 @@
+(function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+e.src='//www.google-analytics.com/analytics.js';
+r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+ga('create','64252817-1','auto');ga('send','pageview');
