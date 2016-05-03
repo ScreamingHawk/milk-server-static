203166101464597da121afb3baf41fe7847e3b1f
Michael Standen <screaminghawk@gmail.com>
2016-05-04 09:33:42 +1200

    Added static content for wedding site

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
 $('.animate-hellip').animateHellip();
+
+function scrollToElement(eId){
+    $('html,body').animate({scrollTop: $("#"+ eId).offset().top},'slow');
+	$('#navmenu').click();
+	return false;
+}
